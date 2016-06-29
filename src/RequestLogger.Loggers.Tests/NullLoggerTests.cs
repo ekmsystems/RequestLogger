5613b27c81cdbdec94f5fab50d3b7ac00c21da18
Steven Atkinson <steven.atkinson@gmx.com>
2016-06-29 11:13:15 +0100

    Added tests for the NullLogger.

---
+++ src/RequestLogger.Loggers.Tests/NullLoggerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RequestLogger.Loggers.Tests
+{
+    [TestFixture]
+    public class NullLoggerTests
+    {
+        private NullLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new NullLogger();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _logger = null;
+        }
+
+        [Test]
+        public void Log_Should_Not_Throw_Exception()
+        {
+            Assert.DoesNotThrow(() => _logger.Log(new RequestData(), new ResponseData()));
+        }
+
+        [Test]
+        public void LogError_Should_Not_Throw_Exception()
+        {
+            Assert.DoesNotThrow(() => _logger.LogError(new RequestData(), new ResponseData(), new Exception()));
+        }
+    }
+}
